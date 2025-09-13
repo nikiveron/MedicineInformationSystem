21f24726c57881e4ad58434536ff24eb60ce7a9c
nikiveron <nikiveron@bk.ru>
2025-09-13 23:57:05 +0400

    added patients frontend page

---
+++ MedicalInformationSystem.Web/PatientsApiClient.cs
@@ -0,0 +1,26 @@
+﻿namespace MedicalInformationSystem.Web;
+
+public record Patient(int id, string Surname, string Name, DateTimeOffset DateOfBirth, string Gender, string Phone);
+
+public class PatientsApiClient(HttpClient httpClient)
+{
+    public async Task<Patient[]> GetPatientsAsync(int maxItems = 10, CancellationToken cancellationToken = default)
+    {
+        List<Patient>? patients = null;
+
+        await foreach (var patient in httpClient.GetFromJsonAsAsyncEnumerable<Patient>("/patients/getPatients", cancellationToken))
+        {
+            if (patients?.Count >= maxItems)
+            {
+                break;
+            }
+            if (patient is not null)
+            {
+                patients ??= [];
+                patients.Add(patient);
+            }
+        }
+
+        return patients?.ToArray() ?? [];
+    }
+}
