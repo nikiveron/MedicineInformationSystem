68b58c4841b3d28a8b6c55ad1a3d9a4130cab3c1
nikiveron <nikiveron@bk.ru>
2025-09-13 23:12:42 +0400

    added patient controller

--- MedicalInformationSystem.ApiService/Controllers/PatientController.cs
+++ MedicalInformationSystem.ApiService/Controllers/PatientController.cs
@@ -1,10 +1,43 @@
 ﻿using MedicalInformationSystem.ApiService.Models;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using MedicalInformationSystem.ApiService.Services;
 
 namespace MedicalInformationSystem.ApiService.Controllers;
 
-public class PatientController : Controller
+[Route("patients")]
+public class PatientController(IPatientRepository patientRepository) : Controller
 {
-    
+    [HttpGet("getPatients")]
+    public async Task<ActionResult<List<PatientEntity>>> Get(CancellationToken cancellationToken)
+    {
+        try
+        {
+            var patientsList = await patientRepository.Get(cancellationToken);
+            return Ok(patientsList);
+        }
+        catch (Exception ex)
+        {
+            return StatusCode(500, $"Внутренняя ошибка сервера: {ex.Message}");
+        }
+    }
+
+    [HttpGet("getPatient/{id}")]
+    public async Task<ActionResult> Get([FromRoute] int id, CancellationToken cancellationToken)
+    {
+        try
+        {
+            var agent = await patientRepository.Get(id, cancellationToken);
+
+            if (agent == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(agent);
+        }
+        catch (Exception ex)
+        {
+            return StatusCode(500, $"Внутренняя ошибка сервера: {ex.Message}");
+        }
+    }
 }
---
+++ MedicalInformationSystem.ApiService/Services/PatientRepository.cs
@@ -0,0 +1,25 @@
+﻿using MedicalInformationSystem.ApiService.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace MedicalInformationSystem.ApiService.Services;
+
+public interface IPatientRepository
+{
+    Task<List<PatientEntity>> Get(CancellationToken cancellationToken);
+    Task<PatientEntity?> Get(int patientId, CancellationToken cancellationToken);
+}
+
+internal class PatientRepository(AppDbContext context) : IPatientRepository
+{
+    public async Task<List<PatientEntity>> Get(CancellationToken cancellationToken)
+    {
+        var result = await context.Patients.ToListAsync(cancellationToken);
+        return result;
+    }
+
+    public async Task<PatientEntity?> Get(int patientId, CancellationToken cancellationToken)
+    {
+        var result = await context.Patients.FindAsync(patientId, cancellationToken);
+        return result;
+    }
+}
